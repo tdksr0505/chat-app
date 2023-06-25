0673bd6117fa10d0380f7de1d5675ea0aa5ef107
Lucas <tdksr0505@gmail.com>
2023-06-25 18:38:19 +0800

    fix: 登入表單新增form標籤

--- src/components/loginView.tsx
+++ src/components/loginView.tsx
@@ -14,9 +14,11 @@ const loginView: React.FC<loginViewProps> = ({ setIsLogin }) => {
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
   }
-  const handleStart = () => {
+  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
     if (!name) {
       alert('暱稱不可為空白')
+
       return
     }
     socket.emit(C2S_COMMAND.USER_JOIN, name)
@@ -38,23 +40,25 @@ const loginView: React.FC<loginViewProps> = ({ setIsLogin }) => {
         <div className="font-Fredericka text-blue-800 text-6xl mb-10 border-b-2 border-blue-300 pb-5">
           Draw & Guess
         </div>
-        <div className="mb-10">
-          <input
-            type="text"
-            value={name}
-            onChange={handleNameChange}
-            placeholder="請輸入暱稱"
-            className=" rounded outline-0 p-2 shadow-md border-2 border-blue-500 w-4/5"
-          />
-        </div>
-        <div>
-          <button
-            onClick={handleStart}
-            className="text-xl  rounded-md  w-4/5 py-2  text-white bg-blue-500 shadow-md shadow-blue-500/50 hover:bg-blue-600"
-          >
-            START
-          </button>
-        </div>
+        <form onSubmit={onFormSubmit}>
+          <div className="mb-10">
+            <input
+              type="text"
+              value={name}
+              onChange={handleNameChange}
+              placeholder="請輸入暱稱"
+              className=" rounded outline-0 p-2 shadow-md border-2 border-blue-500 w-4/5"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="text-xl  rounded-md  w-4/5 py-2  text-white bg-blue-500 shadow-md shadow-blue-500/50 hover:bg-blue-600"
+            >
+              START
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   )
